d5af94f6a8020fc17cd9120fa0cbebc4385c39e9
Jared Bilton <cantops@jareds-mbp.attlocal.net>
2015-10-14 18:30:23 -0400

    Initial Commits

---
+++ main.js
@@ -0,0 +1,20 @@
+confirm("Let's get it on!");
+
+var age = prompt ("What's my age again?");
+if(age < 18){
+    console.log("You may not proceed")
+} else {
+    console.log("You may proceed at leisure!")
+}
+// intro, now game begins//
+
+var user = prompt("You are Rick Grimmes, a former sheriff from Fulton County, GA. You have awoken from a coma and the world as you know it no longer exists.  You have found yourself in a disease ridden zombie apocalypse.  In order to survive, you must avoid being scratched or bitten by zombie walkers. During your journey to reunite with your family, you will encounter the walking dead.  It is up to you to survive.  You must choose for every zombie you encounter to either STAB one with a knife, SHOOT one with a gun, or RUN away")
+
+if(user === STAB) {
+
+console.log("Knife the walker in the head!");
+} else {
+  console.log("Use your gun to shoot one!");
+} else if {
+console.log("Run away Rick!");
+}
